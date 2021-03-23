00177922447566bc021164ace3351a2fd823a140
Mateusz Golombniak <Mejtika@hotmail.com>
2021-03-23 16:44:05 +0100

    Added e2e tests

--- src/frontend/e2e/src/app.e2e-spec.ts
+++ src/frontend/e2e/src/app.e2e-spec.ts
@@ -6,11 +6,24 @@ describe('workspace-project App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    browser.waitForAngularEnabled(false);
   });
 
-  it('should display welcome message', () => {
+  it('should increase the number of items after adding', async (done) => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ToDoListApp!');
+
+    await browser.sleep(5000);
+    var itemsBeforeAdding = await page.getItemsCount();
+
+    page.getInput().sendKeys('sample item');
+    page.getButton().click();
+    await browser.sleep(1000);
+    await browser.refresh();
+
+    await browser.sleep(3000);
+    var itemsAfterAdding = await page.getItemsCount();
+    expect(itemsAfterAdding).toBe(itemsBeforeAdding + 1);
+    done();
   });
 
   afterEach(async () => {
